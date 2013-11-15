226117f52a91a24c23c48c088684d00d46d973a6
Quinn Liu <quinnliu@vt.edu>
2013-11-15 03:37:50 -0500

    initial implementation for feature normalization

--- supervisedLearning/linearRegressionIn1Variable/mlclass-ex1/featureNormalize.m
+++ supervisedLearning/linearRegressionIn1Variable/mlclass-ex1/featureNormalize.m
@@ -7,6 +7,8 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 % You need to set these values correctly
 X_norm = X;
+
+% size(X, 2) = # of columns in matrix X
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
 
@@ -24,15 +26,26 @@ sigma = zeros(1, size(X, 2));
 %               each feature. 
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
+%     
 
+% X(:, 1) = all values of x_1 (house sizes in the training set)
+% X(:, 2) = all values of x_2 (# of bedrooms in the training set)
+meanOfHousePrices = mean(X(:, 1));
+meanOfBedrooms = mean(X(:, 2));
+mu(:, 1) = meanOfHousePrices;
+mu(:, 2) = meanOfBedrooms;
 
+% subtract each feature within the data set with it's mean
+X_norm(:, 1) = X_norm(:, 1) .- meanOfHousePrices;
+X_norm(:, 2) = X_norm(:, 2) .- meanOfBedrooms;
 
+standardDeviationOfHousePrices = std(X(:, 1));
+standardDeviationOfBedrooms = std(X(:, 2));
+sigma(:, 1) = standardDeviationOfHousePrices;
+sigma(:, 2) = standardDeviationOfBedrooms;
 
-
-
-
-
+X_norm(:, 1) = X_norm(:, 1) ./ standardDeviationOfHousePrices;
+X_norm(:, 2) = X_norm(:, 2) ./ standardDeviationOfBedrooms;
 
 % ============================================================
 
