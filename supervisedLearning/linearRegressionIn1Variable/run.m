0a7e64b0c69cfd02a67db7d78b5e1183c2b5e7fa
Quinn Liu <quinnliu@vt.edu>
2013-11-29 02:10:25 -0500

    readded accidentally removed code

--- supervisedLearning/linearRegressionIn1Variable/run.m
+++ supervisedLearning/linearRegressionIn1Variable/run.m
@@ -64,6 +64,15 @@ theta0_vals = linspace(-10, 10, 100);
 theta1_vals = linspace(-1, 4, 100);
 
 % initialize J_vals to a matrix of 0's
+J_vals = zeros(length(theta0_vals), length(theta1_vals));
+
+% Fill out J_vals
+for i = 1:length(theta0_vals)
+    for j = 1:length(theta1_vals)
+          t = [theta0_vals(i); theta1_vals(j)];    
+          J_vals(i,j) = computeCost(X, y, t);
+    end
+end
 
 % Because of the way meshgrids work in the surf command, we need to 
 % transpose J_vals before calling surf, or else the axes will be flipped
