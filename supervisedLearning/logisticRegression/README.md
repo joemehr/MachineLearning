f25b663edcb9e10f61d933255597ec868a920341
Quinn Liu <quinnliu@vt.edu>
2014-01-03 22:20:15 -0500

    implemented sigmoid function

--- supervisedLearning/logisticRegression/README.md
+++ supervisedLearning/logisticRegression/README.md
@@ -23,7 +23,7 @@ Logistic Regression is a popular algorithm used for Classifiction Problems
 
 - Binary Classification = output y is a discrete value of 0 (negative class) or 1 (positive class)
   + 0 <= Logistic Regression output(h_theta(x)) <= 1
-  + h_theta(x) = g(theta^T * x) where g(z) = 1/(1 + e^(-z))
+  + h_theta(x) = g(theta^T * x) where g(z) = 1/(1 + e^(-z)) also known as the sigmoid function
   + Hypothesis Representation = h_theta(x) = 1/(1 + e^(-theta^T * x))
     - h_theta(x) = estimated probability that y = 1 on input x
     - h_theta(x) = P(y = 1 | x; theta)
--- supervisedLearning/logisticRegression/sigmoid.m
+++ supervisedLearning/logisticRegression/sigmoid.m
@@ -1,18 +1,13 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid functoon
+%SIGMOID Compute sigmoid function
 %   J = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
 g = zeros(size(z));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
+% Compute the sigmoid of each value of z (where z can be a matrix,
+% vector or scalar).
 
-
-
-
-
-% =============================================================
+g = 1./(1 + e.^(-z));
 
 end
