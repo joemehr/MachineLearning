b54a30472b296372b250d9af6024e2e43865582b
Quinn Liu <quinnliu@vt.edu>
2014-01-04 15:59:20 -0500

    implemented prediction function

--- supervisedLearning/logisticRegression/ex2.m
+++ supervisedLearning/logisticRegression/ex2.m
@@ -44,27 +44,32 @@ X = [ones(m, 1) X];
 initial_theta = zeros(n + 1, 1);
 
 % Compute and display initial cost and gradient
-[cost, grad] = costFunction(initial_theta, X, y);
+[cost, gradient] = costFunction(initial_theta, X, y);
 
 fprintf('Cost at initial theta (zeros): %f\n', cost);
 fprintf('Gradient at initial theta (zeros): \n');
-fprintf(' %f \n', grad);
-
+fprintf(' %f \n', gradient);
+ 
 fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
 
-%% ============= Part 3: Optimizing using fminunc  =============
-%  In this exercise, you will use a built-in function (fminunc) to find the
-%  optimal parameters theta.
+%% ============= Optimizing using fminunc  =============
+%  use a built-in function (fminunc) to find the
+%  optimal parameters theta. Octave's fminunc is an optimization
+%  solver that finds the minimum of an unconstrained function. 
+%  For logistic regression, you want to optimize the cost
+%  function J(theta) with parameters theta.
 
 %  Set options for fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 
 %  Run fminunc to obtain the optimal theta
 %  This function will return theta and the cost 
-[theta, cost] = ...
-	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+[theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+% NOTE: that by using fminunc, you do not have to write any loops yourself,
+% or set a learning rate like you did for gradient descent. You ONLY need
+% to provide a function calculating the cost and the gradient.
 
 % Print theta to screen
 fprintf('Cost at theta found by fminunc: %f\n', cost);
--- supervisedLearning/logisticRegression/predict.m
+++ supervisedLearning/logisticRegression/predict.m
@@ -9,19 +9,13 @@ m = size(X, 1); % Number of training examples
 % You need to return the following variables correctly
 p = zeros(m, 1);
 
-% ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
-
-% =========================================================================
-
+% X = mxn matrix
+% theta = nx1 column vector
+p = (sigmoid(X*theta) >= 0.5);
 
 end
