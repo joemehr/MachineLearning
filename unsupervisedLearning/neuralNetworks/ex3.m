3a5bd8e71c92b837cf10b73304ffaddcb600338c
Quinn Liu <quinnliu@vt.edu>
2014-01-07 16:44:37 -0500

    adding comments to code

--- unsupervisedLearning/neuralNetworks/ex3.m
+++ unsupervisedLearning/neuralNetworks/ex3.m
@@ -1,45 +1,29 @@
-%% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     lrCostFunction.m (logistic regression cost function)
-%     oneVsAll.m
-%     predictOneVsAll.m
-%     predict.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
+%% One-vs-all
 
 %% Initialization
 clear ; close all; clc
 
 %% Setup the parameters you will use for this part of the exercise
-input_layer_size  = 400;  % 20x20 Input Images of Digits
-num_labels = 10;          % 10 labels, from 1 to 10   
+input_layer_size  = 400;  % 20 pixel x 20 pixel input Images of Digits
+numberOfLabels = 10;          % 10 labels, from 1 to 10   
                           % (note that we have mapped "0" to label 10)
 
-%% =========== Part 1: Loading and Visualizing Data =============
+%% =========== Loading and Visualizing Data =============
 %  We start the exercise by first loading and visualizing the dataset. 
 %  You will be working with a dataset that contains handwritten digits.
-%
 
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
 load('ex3data1.mat'); % training data stored in arrays X, y
 m = size(X, 1);
-
+ 
 % Randomly select 100 data points to display
-rand_indices = randperm(m);
-sel = X(rand_indices(1:100), :);
-
-displayData(sel);
+randomIndices = randperm(m);
+randomlySelectedIndices = X(randomIndices(1:100), :);
+ 
+displayData(randomlySelectedIndices);
+title('Example of 100 Randomly Selected Digits from Input Training Set');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
@@ -55,7 +39,7 @@ pause;
 fprintf('\nTraining One-vs-All Logistic Regression...\n')
 
 lambda = 0.1;
-[all_theta] = oneVsAll(X, y, num_labels, lambda);
+[all_theta] = oneVsAll(X, y, numberOfLabels, lambda);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
