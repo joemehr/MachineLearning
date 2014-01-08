ff8fc7456737b46b6cf0cafd0ea58d17235845f7
Quinn Liu <quinnliu@vt.edu>
2014-01-08 17:58:37 -0500

    implemented neural network with multiple layers

--- unsupervisedLearning/neuralNetworks/predict.m
+++ unsupervisedLearning/neuralNetworks/predict.m
@@ -1,35 +1,31 @@
-function p = predict(Theta1, Theta2, X)
+function predictions = predict(Theta1, Theta2, X)
 %PREDICT Predict the label of an input given a trained neural network
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
 % Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+numberOfTrainingExamples = size(X, 1);
+numberOfLabels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+p = zeros(size(X, 1), 1); % p = m x 1 column vector
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+X = [ones(numberOfTrainingExamples, 1) X];
+% Now X = 5000 x 401 matrix
 
+% a1 = 5000 x 401 matrix
+a1 = X;
 
+% Theta1' = 401 x 25 matrix
+% a2 = 5000 x 25 matrix
+a2 = sigmoid(a1 * Theta1');
+a2 = [ones(numberOfTrainingExamples, 1) a2]; % add an extra column vector of 1's
+% Now a2 = 5000 x 26 matrix
 
+% Theta2' = 26 x 10 matrix
+% a3 = 5000 x 10 matrix
+a3 = sigmoid(a2 * Theta2');
 
-
-
-
-
-
-% =========================================================================
-
+[M, predictions] = max(a3, [], 2);
 
 end
