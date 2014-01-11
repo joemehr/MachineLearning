d2304fb235734bc838110d9de17be4cbce431415
Quinn Liu <quinnliu@vt.edu>
2014-01-10 23:31:52 -0500

    implemented derivative of sigmoid function also known as sigmoid gradient

--- unsupervisedLearning/neuralNetworks/learningWithBackpropagation/sigmoidGradient.m
+++ unsupervisedLearning/neuralNetworks/learningWithBackpropagation/sigmoidGradient.m
@@ -1,33 +1,16 @@
-function g = sigmoidGradient(z)
+function gradientOfSigmoidFunction = sigmoidGradient(z)
 %SIGMOIDGRADIENT returns the gradient of the sigmoid function
 %evaluated at z
-%   g = SIGMOIDGRADIENT(z) computes the gradient of the sigmoid function
+%   gradientOfSigmoidFunction = SIGMOIDGRADIENT(z) computes the gradient of the sigmoid function
 %   evaluated at z. This should work regardless if z is a matrix or a
 %   vector. In particular, if z is a vector or matrix, you should return
 %   the gradient for each element.
 
-g = zeros(size(z));
+gradientOfSigmoidFunction = zeros(size(z));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the gradient of the sigmoid function evaluated at
-%               each value of z (z can be a matrix, vector or scalar).
+% Compute the gradient(derivative) of the sigmoid function evaluated at each value 
+% of z (z can be a matrix, vector or scalar).
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
-
-
+gradientOfSigmoidFunction = sigmoid(z) .* (1 - sigmoid(z));
 
 end
